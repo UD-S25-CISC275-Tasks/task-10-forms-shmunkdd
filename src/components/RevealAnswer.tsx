e3e8aed1679154bd84cc7164dc12aa117f2c6011
Diego Quijada <diegoqui@udel.edu>
2025-03-18 21:55:34 -0400

    saving

--- src/components/RevealAnswer.tsx
+++ src/components/RevealAnswer.tsx
@@ -2,22 +2,5 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function RevealAnswer(): React.JSX.Element {
-<<<<<<< HEAD
-    const [visible, setVisible] = useState<boolean>(false);
-
-    return (
-        <div>
-            <Button
-                onClick={() => {
-                    setVisible(!visible);
-                }}
-            >
-                Reveal Answer
-            </Button>
-            {visible && <p>42</p>}
-        </div>
-    );
-=======
     return <div>Reveal Answer</div>;
->>>>>>> origin/task-forms
 }
