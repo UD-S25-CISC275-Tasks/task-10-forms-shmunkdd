747b3d816b1dacf94d7073133e75e2e81d1aa523
Diego Quijada <diegoqui@udel.edu>
2025-03-19 03:33:38 -0400

    added

--- src/form-components/EditMode.tsx
+++ src/form-components/EditMode.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Form from "react-bootstrap/Form"; // Import Bootstrap Form
 
 export function EditMode(): React.JSX.Element {
     const [editMode, setEditMode] = useState<boolean>(false);
@@ -8,30 +9,31 @@ export function EditMode(): React.JSX.Element {
     return (
         <div>
             <h3>Edit Mode</h3>
-            <label>
-                <input
-                    type="checkbox"
+            {/* Ensuring the checkbox for edit mode is inside a form-switch div */}
+            <div className="form-switch">
+                <Form.Check
+                    type="switch"
+                    id="edit-mode-switch"
+                    label="Edit Mode"
                     checked={editMode}
                     onChange={() => setEditMode(!editMode)}
                 />
-                Edit Mode
-            </label>
+            </div>
 
             {editMode ?
                 <div>
-                    <input
+                    <Form.Control
                         type="text"
                         value={userName}
                         onChange={(e) => setUserName(e.target.value)}
                     />
-                    <label>
-                        <input
-                            type="checkbox"
-                            checked={isStudent}
-                            onChange={() => setIsStudent(!isStudent)}
-                        />
-                        Student
-                    </label>
+                    <Form.Check
+                        type="checkbox"
+                        id="student-checkbox"
+                        label="Student"
+                        checked={isStudent}
+                        onChange={() => setIsStudent(!isStudent)}
+                    />
                 </div>
             :   <p>
                     {userName} is {isStudent ? "a student" : "not a student"}.
