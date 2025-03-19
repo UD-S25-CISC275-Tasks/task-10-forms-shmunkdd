9662fefa052cea1e0c8fb8a4406078202a18dc56
Diego Quijada <diegoqui@udel.edu>
2025-03-18 21:49:03 -0400

    saving

--- src/bad-components/ChooseTeam.tsx
+++ src/bad-components/ChooseTeam.tsx
@@ -11,19 +11,6 @@ const PEOPLE = [
 ];
 
 export function ChooseTeam(): React.JSX.Element {
-<<<<<<< HEAD
-    const [team, setTeam] = useState<string[]>([]);
-
-    function chooseMember(person: string) {
-        const addPerson = `Add ${person}`;
-        setTeam((prevTeam) =>
-            prevTeam.includes(addPerson) ? prevTeam : [...prevTeam, addPerson],
-        );
-    }
-
-    function clearTeam() {
-        setTeam([]); // Properly reset the state
-=======
     const [allOptions, setAllOptions] = useState<string[]>(PEOPLE);
     const [team, setTeam] = useState<string[]>([]);
 
@@ -39,7 +26,6 @@ export function ChooseTeam(): React.JSX.Element {
         /*
         team = [];
         */
->>>>>>> origin/task-forms
     }
 
     return (
@@ -47,40 +33,20 @@ export function ChooseTeam(): React.JSX.Element {
             <h3>Choose Team</h3>
             <Row>
                 <Col>
-<<<<<<< HEAD
-                    {PEOPLE.map((option: string) => (
-                        <div key={option} style={{ marginBottom: "4px" }}>
-                            <Button
-                                onClick={() => {
-                                    chooseMember(option);
-                                }}
-                                size="sm"
-                            >
-                                Add {option}
-=======
                     {allOptions.map((option: string) => (
                         <div key={option} style={{ marginBottom: "4px" }}>
                             Add{" "}
                             <Button onClick={chooseMember} size="sm">
                                 {option}
->>>>>>> origin/task-forms
                             </Button>
                         </div>
                     ))}
                 </Col>
                 <Col>
                     <strong>Team:</strong>
-<<<<<<< HEAD
-                    <ul>
-                        {team.map((member: string) => (
-                            <li key={member}>{member}</li>
-                        ))}
-                    </ul>
-=======
                     {team.map((member: string) => (
                         <li key={member}>{member}</li>
                     ))}
->>>>>>> origin/task-forms
                     <Button onClick={clearTeam}>Clear Team</Button>
                 </Col>
             </Row>
