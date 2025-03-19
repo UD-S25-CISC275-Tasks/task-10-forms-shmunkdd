0ac8d6167f1e9dcd183799a41d5fcc308b5acb0b
Diego Quijada <diegoqui@udel.edu>
2025-03-18 22:15:52 -0400

    added

--- src/form-components/EditMode.tsx
+++ src/form-components/EditMode.tsx
@@ -1,9 +1,42 @@
 import React, { useState } from "react";
 
 export function EditMode(): React.JSX.Element {
+    const [editMode, setEditMode] = useState<boolean>(false);
+    const [userName, setUserName] = useState<string>("Your Name");
+    const [isStudent, setIsStudent] = useState<boolean>(true);
+
     return (
         <div>
             <h3>Edit Mode</h3>
+            <label>
+                <input
+                    type="checkbox"
+                    checked={editMode}
+                    onChange={() => setEditMode(!editMode)}
+                />
+                Edit Mode
+            </label>
+
+            {editMode ?
+                <div>
+                    <input
+                        type="text"
+                        value={userName}
+                        onChange={(e) => setUserName(e.target.value)}
+                    />
+                    <label>
+                        <input
+                            type="checkbox"
+                            checked={isStudent}
+                            onChange={() => setIsStudent(!isStudent)}
+                        />
+                        Student
+                    </label>
+                </div>
+            :   <p>
+                    {userName} is {isStudent ? "a student" : "not a student"}.
+                </p>
+            }
         </div>
     );
 }
