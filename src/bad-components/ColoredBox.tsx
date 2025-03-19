76b343f7f5aa769c9f82c58b7d5ed4b6f3c0b007
Diego Quijada <diegoqui@udel.edu>
2025-03-18 21:51:14 -0400

    saving

--- src/bad-components/ColoredBox.tsx
+++ src/bad-components/ColoredBox.tsx
@@ -4,24 +4,12 @@ import { Button } from "react-bootstrap";
 export const COLORS = ["red", "blue", "green"];
 const DEFAULT_COLOR_INDEX = 0;
 
-<<<<<<< HEAD
-function ChangeColor({
-    setColorIndex,
-}: {
-    setColorIndex: (index: number) => void;
-}): React.JSX.Element {
-    return (
-        <Button
-            onClick={() => {
-                setColorIndex((prevIndex) => (prevIndex + 1) % COLORS.length);
-=======
 function ChangeColor(): React.JSX.Element {
     const [colorIndex, setColorIndex] = useState<number>(DEFAULT_COLOR_INDEX);
     return (
         <Button
             onClick={() => {
                 setColorIndex((1 + colorIndex) % COLORS.length);
->>>>>>> origin/task-forms
             }}
         >
             Next Color
@@ -29,22 +17,14 @@ function ChangeColor(): React.JSX.Element {
     );
 }
 
-<<<<<<< HEAD
-function ColorPreview({ color }: { color: string }): React.JSX.Element {
-=======
 function ColorPreview(): React.JSX.Element {
->>>>>>> origin/task-forms
     return (
         <div
             data-testid="colored-box"
             style={{
                 width: "50px",
                 height: "50px",
-<<<<<<< HEAD
-                backgroundColor: color, // Use the passed color
-=======
                 backgroundColor: COLORS[DEFAULT_COLOR_INDEX],
->>>>>>> origin/task-forms
                 display: "inline-block",
                 verticalAlign: "bottom",
                 marginLeft: "5px",
@@ -54,17 +34,6 @@ function ColorPreview(): React.JSX.Element {
 }
 
 export function ColoredBox(): React.JSX.Element {
-<<<<<<< HEAD
-    const [colorIndex, setColorIndex] = useState<number>(DEFAULT_COLOR_INDEX);
-
-    return (
-        <div>
-            <h3>Colored Box</h3>
-            <span>The current color is: {COLORS[colorIndex]}</span>
-            <div>
-                <ChangeColor setColorIndex={setColorIndex} />
-                <ColorPreview color={COLORS[colorIndex]} />
-=======
     return (
         <div>
             <h3>Colored Box</h3>
@@ -72,7 +41,6 @@ export function ColoredBox(): React.JSX.Element {
             <div>
                 <ChangeColor></ChangeColor>
                 <ColorPreview></ColorPreview>
->>>>>>> origin/task-forms
             </div>
         </div>
     );
