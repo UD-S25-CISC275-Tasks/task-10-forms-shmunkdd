1a327e95de1ee1da145acd5f86e10b671c16fd30
Diego Quijada <diegoqui@udel.edu>
2025-03-19 02:26:09 -0400

    bug fixes

--- src/bad-components/DoubleHalf.tsx
+++ src/bad-components/DoubleHalf.tsx
@@ -1,49 +1,27 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
-function Doubler({
-    setValue,
-}: {
-    setValue: (newValue: number) => void;
-}): React.JSX.Element {
-    return (
-        <Button
-            onClick={() => {
-                setValue((prevValue) => 2 * prevValue);
-            }}
-        >
-            Double
-        </Button>
-    );
-}
-
-function Halver({
-    setValue,
-}: {
-    setValue: (newValue: number) => void;
-}): React.JSX.Element {
-    return (
-        <Button
-            onClick={() => {
-                setValue((prevValue) => 0.5 * prevValue);
-            }}
-        >
-            Halve
-        </Button>
-    );
-}
-
 export function DoubleHalf(): React.JSX.Element {
-    const [dhValue, setDhValue] = useState<number>(10); // Correctly place useState inside the component
+    const [value, setValue] = useState<number>(1); // Explicitly set type to number
 
     return (
         <div>
-            <h3>Double Half</h3>
-            <div>
-                The current value is: <span>{dhValue}</span>
-            </div>
-            <Doubler setValue={setDhValue} />
-            <Halver setValue={setDhValue} />
+            <h3>Double and Halve</h3>
+            <p>Value: {value}</p>
+            <Button
+                onClick={() => {
+                    setValue((prevValue: number) => 2 * prevValue);
+                }}
+            >
+                Double
+            </Button>
+            <Button
+                onClick={() => {
+                    setValue((prevValue: number) => 0.5 * prevValue);
+                }}
+            >
+                Halve
+            </Button>
         </div>
     );
 }
